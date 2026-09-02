52cf1551afef155f4b028e884ff7c4c2f6c0eb5c
Sudeep <sudeepsinha337@gmail.com>
2026-09-03 01:04:02 +0530

    01

---
+++ typeCasting.java
@@ -0,0 +1,12 @@
+public class typeCasting {
+    public static void main (String[] args){
+        int a = 257;
+        byte b = (byte)a;
+
+        float f = 5.6f;
+        int t = (int) f;
+
+        System.out.println(b);
+        System.out.println(t);
+    }
+}
---
+++ variable.java
@@ -0,0 +1,22 @@
+public class variable {
+    public static void main ( String[] args){
+
+        int num1 = 6;
+        int num2 = 10;
+         int result = num1 + num2;
+
+        byte by = 127;
+        short sh = 227;
+        long l = 5598l;
+
+        float f = 5.6f;
+        double d = 5.6;
+
+        char c = 'a';
+        c++;
+
+        boolean b = true;
+        System.out.println("num = " + result);
+        System.out.println(c);
+    }
+}
